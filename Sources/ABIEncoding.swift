0f7d0f4f6f4b1b071e9bf9a6c5dfdd6320693510
ccxdd <e_xiaodong@hotmail.com>
2018-11-18 21:27:48 +0800

    podspec

---
+++ Sources/ABIEncoding.swift
@@ -0,0 +1,299 @@
+//
+//  ABIEncoding.swift
+//  ERC Tools
+//
+//  Created by 陈晓东 on 2018/08/20.
+//  Copyright © 2018年 陈晓东. All rights reserved.
+//
+
+import Foundation
+
+public enum InputSolidityType {
+    case address(String)
+    case bytes(Array<UInt8>)
+    case bytesFixed(Int, Array<UInt8>)
+    /// = uint8 value 0,1
+    case bool(Bool)
+    case string(String)
+    /// = uint256
+    case uint(UInt)
+    case uint8(UInt8)
+    case uint32(UInt32)
+    case uint128(UInt)
+    case uint256(UInt)
+    case uintArray([UInt])
+    case uint8Array([UInt])
+    case uint32Array([UInt])
+    case uint128Array([UInt])
+    case uint256Array([UInt])
+    case addressArray([String])
+    
+    var isDynamic: Bool {
+        switch self {
+        case .string(_), .bytes(_), .uintArray(_), .uint8Array(_), .uint32Array(_), .uint128Array(_), .uint256Array(_), .addressArray(_):
+            return true
+        default:
+            return false
+        }
+    }
+    
+    /// 位置 + 长度 + 数组中每个值
+    var lines: Int {
+        switch self {
+        case .string(_), .bytes(_), .uintArray(_), .uint8Array(_), .uint32Array(_), .uint128Array(_), .uint256Array(_), .addressArray(_):
+            return 2 + dataLines
+        default:
+            return 1
+        }
+    }
+    
+    /// 数据行
+    var dataLines: Int {
+        switch self {
+        case .string(let s):
+            let dataCount = (s.data(using: .utf8)?.count ?? 0)
+            return dataCount / 32 + (dataCount % 32 == 0 ? 0 : 1)
+        case .bytes(let s):
+            return s.count / 32 + (s.count % 32 == 0 ? 0 : 1)
+        case .uintArray(let a), .uint8Array(let a), .uint32Array(let a), .uint128Array(let a), .uint256Array(let a):
+            return a.count
+        case .addressArray(let a):
+            return a.count
+        default:
+            return 1
+        }
+    }
+    
+    var typeLength: Int {
+        switch self {
+        case .string(let s):
+            let dataCount = (s.data(using: .utf8)?.count ?? 0)
+            return dataCount
+        case .bytes(let s):
+            return s.count
+        case .uintArray(let a), .uint8Array(let a), .uint32Array(let a), .uint128Array(let a), .uint256Array(let a):
+            return a.count
+        default:
+            return 0
+        }
+    }
+    
+    var typeData: String {
+        switch self {
+        case .string(let s):
+            return s.hexString.fill0(len: 64 * dataLines, left: false)
+        case .bytes(let s):
+            return s.toHexString().fill0(len: 64 * dataLines, left: false)
+        case .bytesFixed(let l, let bytes):
+            let b = bytes.range(from: 0, to: min(l - 1 , 31)) ?? []
+            return b.toHexString().fill0(len: 64, left: false)
+        case .address(let s):
+            return s.clearAddressPrefix
+        case .bool(let b):
+            return (b ? 1 : 0).radix(16, len: 64)
+        case .uint8(let i):
+            return Int(i).radix(16, len: 64)
+        case .uint32(let i):
+            return Int(i).radix(16, len: 64)
+        case .uint128(let i), .uint256(let i), .uint(let i):
+            return Int(i).radix(16, len: 64)
+        case .uint8Array(let a), .uint32Array(let a), .uint128Array(let a), .uint256Array(let a), .uintArray(let a):
+            var strArr: [String] = []
+            for i in a {
+                let v = Int(i).radix(16, len: 64)
+                strArr.append(v)
+            }
+            return strArr.joined(separator: "")
+        case .addressArray(let a):
+            var strArr: [String] = []
+            for i in a {
+                strArr.append(i.clearAddressPrefix)
+            }
+            return strArr.joined(separator: "")
+        }
+    }
+    
+    var typeEncoding: SolidityTypeEncoding {
+        var type = SolidityTypeEncoding()
+        if isDynamic {
+            type.length = typeLength.radix(16, len: 64)
+        }
+        type.data = typeData
+        return type
+    }
+    
+    var desc: String {
+        switch self {
+        case .string(_):
+            return "string"
+        case .address(_):
+            return "address"
+        case .bytes(_):
+            return "bytes"
+        case .bytesFixed(let l, _):
+            return "bytes\(min(32, l))"
+        case .bool(_):
+            return "bool"
+        case .uint8(_):
+            return "uint8"
+        case .uint32(_):
+            return "uint32"
+        case .uint128(_):
+            return "uint128"
+        case .uint(_), .uint256(_):
+            return "uint256"
+        case .uint8Array(_):
+            return "uint8[]"
+        case .uint32Array(_):
+            return "uint32[]"
+        case .uint128Array(_):
+            return "uint128[]"
+        case .uintArray(_), .uint256Array(_):
+            return "uint256[]"
+        case .addressArray:
+            return "address[]"
+        }
+    }
+}
+
+public enum OutputSolidityType {
+    case string
+    case bytes
+    case bytesFixed(Int)
+    case bool
+    case int
+    case address
+    
+    func decoding(total: Data, idx: Int) -> Data {
+        let start = idx * 32
+        switch self {
+        case .bytesFixed(let l):
+            return total[start ..< start + l]
+        case .bool, .int:
+            return total[start ..< start + 32]
+        case .string, .bytes:
+            guard let s = total[start ..< start + 32].hex().hexToInt,
+                let len = total[s ..< s + 32].hex().hexToInt, len > 0
+                else { return Data() }
+            let s2 = s + 32
+            return total[s2 ..< s2 + len]
+        case .address:
+            return total[start + 12 ..< start + 32]
+        }
+    }
+}
+
+public struct SolidityReturnDecode {
+    var dataArray: [Data] = []
+    var arguments: [OutputSolidityType] = []
+    
+    func get<T>(index: Int, type: T.Type) -> T? where T: Decodable {
+        guard !dataArray.isEmpty else { return nil }
+        let i = min(index, dataArray.count - 1)
+        let v = dataArray.at(i)!
+        let t = arguments.at(i)!
+        var result: Decodable
+        switch t {
+        case .string:
+            result = v.tString
+        case .int:
+            result = v.hex().hexToInt
+        case .bool:
+            result = (v.hex().hexToInt == 0 ? false : true)
+        case .bytesFixed(_), .bytes:
+            result = v.bytes
+        case .address:
+            result = "0x" + v.hex()
+        }
+        return result as? T
+    }
+    
+    func model<T>(type: T.Type) -> T? where T: SolidityModelProtocol {
+        return T.converModel(self)
+    }
+}
+
+public struct ABIFunc {
+    
+    static func call(name: String, arguments: [InputSolidityType] = []) -> ABIFunc {
+        return ABIFunc(name: name, arguments: arguments)
+    }
+    
+    private init(name: String, arguments: [InputSolidityType] = []) {
+        self.name = name
+        self.arguments = arguments
+    }
+    
+    var name: String
+    
+    var arguments: [InputSolidityType] = []
+    
+    var totalLines: Int {
+        let lines = arguments.reduce(0, { $0 + $1.lines })
+        return lines
+    }
+    
+    var funcSign: String {
+        var signatureArr = [name, "("]
+        for (idx, i) in arguments.enumerated() {
+            if idx == 0 {
+                signatureArr.append(i.desc)
+            } else {
+                signatureArr.append("," + i.desc)
+            }
+        }
+        signatureArr.append(")")
+        let funcSign = signatureArr.joined(separator: "").sha3(.keccak256).subTo(8)?.prefix("0x") ?? ""
+        return funcSign
+    }
+    
+    var encoding: String {
+        let argumentCount = arguments.count
+        var prevDataLines = 0
+        var encodingDataArr: [String] = [funcSign]
+        var dynamicTypeArr: [SolidityTypeEncoding] = []
+        /// 保存动态类型的头部及非动态类型的值
+        for i in arguments {
+            var type = i.typeEncoding
+            if i.isDynamic {
+                /// 第2行到当前type的数据行
+                let head = (argumentCount + prevDataLines) * 32
+                type.head = head.radix(16, len: 64)
+                encodingDataArr.append(type.head!)
+                dynamicTypeArr.append(type)
+                prevDataLines += (1 + i.dataLines)
+            } else {
+                encodingDataArr.append(type.data)
+            }
+        }
+        /// 保存动态类型的长度和值
+        for i in dynamicTypeArr {
+            encodingDataArr.append(i.length!)
+            encodingDataArr.append(i.data)
+        }
+        return encodingDataArr.joined(separator: "")
+    }
+    
+    static func decoding(_ r: String, arguments: OutputSolidityType...) -> SolidityReturnDecode {
+        let d = Data(hex: r)
+        var dataArr: [Data] = []
+        guard d.count % 32 == 0, d.count > 0 else { return SolidityReturnDecode() }
+        for (idx, i) in arguments.enumerated() {
+            dataArr.append(i.decoding(total: d, idx: idx))
+        }
+        return SolidityReturnDecode(dataArray: dataArr, arguments: arguments)
+    }
+}
+
+struct SolidityTypeEncoding {
+    var head: String?
+    var length: String?
+    var data: String = ""
+}
+
+extension String {
+    var clearAddressPrefix: String {
+        let result = hasPrefix("0x") ? self[index(startIndex, offsetBy: 2)...].tS.lowercased() : lowercased()
+        return result.fill0(len: 64)
+    }
+}
---
+++ Sources/EthereumRPC.swift
@@ -0,0 +1,244 @@
+//
+//  EthereumRPC
+//  ERCTools
+//
+//  Created by 陈晓东 on 2018/08/24.
+//  Copyright © 2018 陈晓东. All rights reserved.
+//
+
+import Foundation
+import Starscream
+import SwiftyJSON
+
+public final class EthereumRPC: WebSocketDelegate {
+    private static let shared = EthereumRPC()
+    var rpcSocket: WebSocket!
+    var waitingMethods: [Int: Response] = [:]
+    var changeStatusClosure: ((Bool) -> Void)?
+    
+    private init() {}
+    
+    static var isConnected: Bool {
+        return shared.rpcSocket.isConnected
+    }
+    
+    static func connect(network: Network, changeStatus: ((Bool) -> Void)? = nil) {
+        let urlStr = "wss://\(network.rawValue).infura.io/ws"
+        shared.rpcSocket = WebSocket(url: URL(string: urlStr)!)
+        shared.rpcSocket.delegate = shared
+        shared.changeStatusClosure = changeStatus
+        shared.rpcSocket.connect()
+    }
+    
+    static func eth_call(data: String, to: String? = nil, from: String? = nil) -> Response {
+        let p = Request.ParamItem(from: from, to: to, data: data)
+        return generate(request: Request(method: "eth_call", params: [.dict(p), .string("latest")], id: 0))
+    }
+    
+    static func eth_getBalance(addr: String) -> Response {
+        return generate(request: Request(method: "eth_getBalance", params: [.string(addr), .string("latest")], id: 0))
+    }
+    
+    static func eth_getTransactionCount(addr: String) -> Response {
+        return generate(request: Request(method: "eth_getTransactionCount", params: [.string(addr), .string("latest")], id: 0))
+    }
+    
+    static func eth_getTransactionReceipt(tx: String) -> Response {
+        return generate(request: Request(method: "eth_getTransactionReceipt", params: [.string(tx)], id: 0))
+    }
+    
+    static func sendRawTransaction(tx: String) -> Response {
+        return generate(request: Request(method: "eth_sendRawTransaction", params: [.string(tx)], id: 0))
+    }
+    
+    static func eth_gasPrice() -> Response {
+        return generate(request: Request(method: "eth_gasPrice", params: [], id: 0))
+    }
+    
+    private static func generate(request: Request) -> Response {
+        var req = request
+        let id = Int.random(in: 1 ..< 1_000_000)
+        req.id = id
+        let rpcResp = Response()
+        rpcResp.request = req
+        return rpcResp
+    }
+    
+    private func responseResult<T>(model: T.Type, text: String) where T: Codable {
+        guard let resp = text.data(using: .utf8)?.tModel(JSONRPCResult<T>.self), let result = resp.result else { return }
+        let rpcResp = waitingMethods.removeValue(forKey: resp.id)
+        rpcResp?.ctrl?.isUserInteractionEnabled = true
+        print("✔️", rpcResp?.request.method ?? "", text)
+        if let r = result as? String {
+            if r != "0x" {
+                rpcResp?.stringClosure?(r)
+            } else {
+                rpcResp?.errorClosure?()
+            }
+        } else if let r = result as? TxReceipt {
+            rpcResp?.stringClosure?(r.status)
+        } else {
+            rpcResp?.errorClosure?()
+        }
+    }
+    
+    // MARK: - WebSocketDelegate
+    public func websocketDidConnect(socket: WebSocketClient) {
+        print("INFURA 🤝")
+        changeStatusClosure?(true)
+    }
+    
+    public func websocketDidDisconnect(socket: WebSocketClient, error: Error?) {
+        if let err = error {
+            print("INFURA 😫", err.localizedDescription)
+            changeStatusClosure?(false)
+            waitingMethods.removeAll()
+            DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 5) {
+                socket.connect()
+            }
+        }
+    }
+    
+    public func websocketDidReceiveMessage(socket: WebSocketClient, text: String) {
+        let json = JSON(parseJSON: text)
+        if json["result"].stringValue.count > 0 {
+            responseResult(model: String.self, text: text)
+        } else if json["result"].dictionary != nil {
+            responseResult(model: TxReceipt.self, text: text)
+        } else {
+            print("❌", text)
+            let resp = waitingMethods.removeValue(forKey: json["id"].intValue)
+            resp?.ctrl?.isUserInteractionEnabled = true
+            guard let msg = json["error"]["message"].string else { return }
+            Hint.flash(.labeledError(title: nil, subtitle: msg), delay: 2)
+        }
+    }
+    
+    public func websocketDidReceiveData(socket: WebSocketClient, data: Data) {}
+}
+
+extension EthereumRPC {
+    public enum Network: String {
+        case mainnet
+        case ropsten
+        case rinkeby
+        
+        var chainID: Int {
+            switch self {
+            case .mainnet: return 1
+            case .ropsten: return 3
+            case .rinkeby: return 4
+            }
+        }
+    }
+    
+    public enum Exception: Error {
+        case requestDataError
+        case networkNotConnect
+        case unknown
+    }
+    
+    public struct Request: Codable {
+        let jsonrpc = "2.0"
+        var method = "eth_call"
+        var params: [ParamValue] = []
+        var id: Int = 0
+        
+        struct ParamItem: Codable {
+            var from: String?
+            var to: String?
+            var data: String?
+        }
+        
+        enum ParamValue: Codable {
+            case dict(ParamItem)
+            case string(String)
+            
+            public func encode(to encoder: Encoder) throws {
+                var container = encoder.singleValueContainer()
+                switch self {
+                case .dict(let d):
+                    try container.encode(d)
+                    return
+                case .string(let s):
+                    try container.encode(s)
+                    return
+                }
+            }
+            
+            public init(from decoder: Decoder) throws {
+                let container = try decoder.singleValueContainer()
+                if let v = try? container.decode(ParamItem.self) {
+                    self = .dict(v)
+                } else if let v = try? container.decode(String.self) {
+                    self = .string(v)
+                } else {
+                    throw DecodingError.typeMismatch(ParamValue.self, DecodingError.Context(codingPath: container.codingPath, debugDescription: "Not a JSON"))
+                }
+            }
+        }
+    }
+    
+    public class Response {
+        var request: Request!
+        fileprivate var stringClosure: ((String) -> Void)?
+        fileprivate var errorClosure: (() -> Void)?
+        fileprivate var ctrl: UIView?
+        
+        @discardableResult func responseString(_ completion: @escaping (String) -> Void) -> Self {
+            stringClosure = completion
+            if shared.rpcSocket.isConnected {
+                try! startWrite()
+            } else {
+                shared.rpcSocket.connect()
+            }
+            return self
+        }
+        
+        @discardableResult func error(_ closure: @escaping () -> Void) -> Self {
+            errorClosure = closure
+            return self
+        }
+        
+        @discardableResult func ctrl(_ c: UIView?) -> Self {
+            ctrl = c
+            ctrl?.isUserInteractionEnabled = false
+            return self
+        }
+        
+        private func startWrite() throws {
+            guard let reqData = request.tJSONString()?.data(using: .utf8) else { throw Exception.requestDataError }
+            shared.waitingMethods[request.id] = self
+            shared.rpcSocket.write(data: reqData)
+        }
+    }
+    
+    public struct JSONRPCResult<T>: Codable where T: Codable {
+        let jsonrpc: String
+        let id: Int
+        var result: T?
+    }
+    
+    public struct TxReceipt: Codable {
+        var status: String
+    }
+}
+
+/* Eg
+ {
+ "jsonrpc": "2.0",
+ "method": "eth_call",
+ "params": [{
+ "from": "0xb60e8dd61c5d32be8058bb8eb970870f07233155",
+ "to": "0xd46e8dd67c5d32be8058bb8eb970870f07244567",
+ "data": "0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675"
+ }, "latest"],
+ "id": 1
+ }
+ 
+ {
+ "jsonrpc": "2.0",
+ "id": 18,
+ "result": "0x00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000000"
+ }
+ */
